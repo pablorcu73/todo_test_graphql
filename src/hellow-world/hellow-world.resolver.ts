912baee0b6e90054dcc1070f9f81abd01e3150fe
Pablo Campoverde <pcampoverde73@hotmail.com>
2023-08-22 10:05:21 -0500

    Query with arguments

--- src/hellow-world/hellow-world.resolver.ts
+++ src/hellow-world/hellow-world.resolver.ts
@@ -1,10 +1,25 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Float, Int, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HellowWorldResolver {
+  @Query(() => String)
+  hellowWorl(): string {
+    return 'Hola mundo';
+  }
 
-    @Query(()=> String)
-    hellowWorl(): string {
-        return 'Hola mundo';
-    }
+  @Query(() => Float, { name: 'randonNumber' })
+  getRandonNumber(): number {
+    return Math.random() * 100;
+  }
+
+  @Query(() => Int, {
+    name: 'randonFromZeroTo',
+    description: 'From zero to argument TO (default 6)',
+  })
+  // request with argument
+  getRandonNumberFromZeroTo(
+    @Args('to', { nullable: true, type: () => Int }) to: number = 6,
+  ): number {
+    return Math.floor(Math.random() * to);
+  }
 }
